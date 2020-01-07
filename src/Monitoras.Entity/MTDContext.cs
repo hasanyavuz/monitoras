da796bdd7570cba36b398d163a00146ebbd5e32e
Hasan Yavuz <hyavuz@bimser.com.tr>
2020-01-07 11:23:56 +0300

    Layout changed with theme defaults.

--- src/Monitoras.Entity/MTDContext.cs
+++ src/Monitoras.Entity/MTDContext.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Monitoras.Entity {
-    public class MTDContext : IdentityDbContext<MTDUser>
+    public class MTDContext : IdentityDbContext<MTDUser, IdentityRole<Guid>, Guid>
     {
         public MTDContext(DbContextOptions options) : base(options)
         {
--- src/Monitoras.Entity/Objects/MTDUser.cs
+++ src/Monitoras.Entity/Objects/MTDUser.cs
@@ -1,8 +1,9 @@
+using System;
 using Microsoft.AspNetCore.Identity;
 
 namespace Monitoras.Entity
 {
-    public class MTDUser : IdentityUser
+    public class MTDUser : IdentityUser<Guid>
     {
         
     }
