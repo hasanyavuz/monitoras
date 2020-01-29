6a6bd6b606456b3e1c255cca04d05ca7ea0046e9
Hasan Yavuz <hyavuz@bimser.com.tr>
2020-01-29 17:03:16 +0300

    Added new route.

--- src/Monitoras.Web/ClientApp/router/routes.js
+++ src/Monitoras.Web/ClientApp/router/routes.js
@@ -1,11 +1,13 @@
 import MonitoringList from 'components/pages/monitoring/list'
 import MonitoringSave from 'components/pages/monitoring/save'
 import Forbidden from 'components/root/forbidden'
+import MonitoringView from 'components/pages/monitoring/view'
 
 export const routes = [
   { name: 'monitoring-list', path: '/monitoring/list', component: MonitoringList, display: 'Monitoring', icon: 'chart-line' },
   { divider: true, path: ''},
   { name: 'monitoring-save', path: '/monitoring/save', component: MonitoringSave, display: 'New Monitoring', icon: 'plus' },
+  { name: 'monitoring-view', path: '/monitoring/view/:id', component: MonitoringView, display: 'View Monitoring', hidden: true },
   { divider: true, path: ''},
   { name: 'account-view', path: '/Identity/Account/Manage', display: 'Account', icon: 'user-circle' },
   { name: 'account-subscription', path: '/Subscription', display: 'Subscription', icon: 'credit-card' },
