d6cad51b561da8ab77a99f9b4dd109178ed27e14
Hasan Yavuz <hyavuz@bimser.com.tr>
2020-01-29 17:03:56 +0300

    Implemented backend monitoring get api.

--- src/Monitoras.Web/ClientApp/services/monitoring.js
+++ src/Monitoras.Web/ClientApp/services/monitoring.js
@@ -10,6 +10,17 @@ const MonitoringService = {
             throw result.error;
           }
     },
+
+    async get(id){
+      var result = await http.get("/api/monitoring/" + id);
+      if (result.status == 200) {
+          return result.data;
+        } else {
+          console.error(result.error);
+          throw result.error;
+        }
+    },
+
     async save(value){
         var result = await http.post("/api/monitoring", value);
         if (result.status == 200) {
