c66b7d5b6608101031e1820afa7db98e7e608474
Hasan Yavuz <hyavuz@bimser.com.tr>
2020-01-29 16:58:59 +0300

    Added get for single item and Fixed UserId problem.

--- src/Monitoras.Web/Controllers/MonitoringController.cs
+++ src/Monitoras.Web/Controllers/MonitoringController.cs
@@ -4,28 +4,67 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Monitoras.Entity;
+using Newtonsoft.Json;
 
 namespace Monitoras.Web.Controllers {
     public class MonitoringController : ApiController {
-        [HttpGet]
-        public async Task<IActionResult> Get () {
+
+        [HttpGet ("{id?}")]
+        public async Task<IActionResult> Get ([FromRoute] Guid? id) {
+            if (id.HasValue) {
+                if (id.Value == Guid.Empty) {
+                    return Error ("You must send monitor id to get.");
+                }
+
+                var monitor = await Db.Monitors.FirstOrDefaultAsync (m => m.MonitorId == id.Value && m.UserId == UserId);
+                if (monitor == null) {
+                    return Error ("Monitor not found.", code : 404);
+                }
+
+                return Success (data: new {
+                    monitor.MonitorId,
+                        monitor.CreatedDate,
+                        monitor.LastCheckDate,
+                        monitor.MonitorStatus,
+                        monitor.Name,
+                        monitor.TestStatus,
+                        monitor.UpTime,
+                        monitor.UpdatedDate
+                });
+            }
+
             var list = await Db.Monitors.ToListAsync ();
             return Success (null, list);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post ([FromBody] MTDMonitor value) {
+        public async Task<IActionResult> Post ([FromBody] MTMMonitorSave value) {
             if (string.IsNullOrEmpty (value.Name)) {
                 return Error ("Name is required.");
             }
 
             var dataObject = new MTDMonitor {
+                MonitorId = Guid.NewGuid (),
                 CreatedDate = DateTime.UtcNow,
-                Name = value.Name
+                Name = value.Name,
+                UserId = UserId
+            };
+            Db.Monitors.Add (dataObject);
+
+            var monitorStepData = new MTDSMonitorStepSettingsRequest {
+                Url = value.Url
             };
 
-            Db.Monitors.Add (dataObject);
+            var monitorStep = new MTDMonitorStep {
+                MonitorStepId = Guid.NewGuid (),
+                Type = MTDMonitorStepTypes.Request,
+                MonitorId = dataObject.MonitorId,
+                Settings = JsonConvert.SerializeObject (monitorStepData)
+            };
+            Db.MonitorSteps.Add (monitorStep);
+
             var result = await Db.SaveChangesAsync ();
+
             if (result > 0)
                 return Success ("Monitoring saved successfully.", new {
                     Id = dataObject.MonitorId
