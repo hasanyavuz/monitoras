461d01b70de6beaeb54c9a5c2290c6647f70897c
Hasan Yavuz <hyavuz@bimser.com.tr>
2020-01-29 17:00:02 +0300

    Added vue loader for fancy pages.

--- src/Monitoras.Web/ClientApp/app.js
+++ src/Monitoras.Web/ClientApp/app.js
@@ -8,11 +8,21 @@ import { FontAwesomeIcon } from './icons'
 import PageHead from 'components/shared/page-head'
 import Notifications from 'vue-notification'
 
+import VueContentPlaceholders from 'vue-content-placeholders'
+
+Vue.use(VueContentPlaceholders)
+
+// Input Controls
+import MTIText from 'components/input/text'
+
 Vue.use(Notifications)
 
 // Registration of global components
 Vue.component('icon', FontAwesomeIcon)
 Vue.component('page-head', PageHead)
+
+// Registration for input controls
+Vue.component('mti-text', MTIText)
 
 Vue.prototype.$http = axios
 
