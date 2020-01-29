07c870902f61cd3882e07f82479e3491e04bd879
Hasan Yavuz <hyavuz@bimser.com.tr>
2020-01-29 17:01:28 +0300

    Added new component.

---
+++ src/Monitoras.Web/ClientApp/components/input/text.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card mb-4">
+    <h6 class="card-header">
+      <div>{{title}}</div>
+    </h6>
+    <div class="card-body">
+      <input
+        type="text"
+        class="form-control"
+        :placeholder="placeholder"
+        v-on:input="$emit('input',$event.target.value)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      default: "",
+      type: String
+    },
+    title: {
+      default: "",
+      type: String
+    }
+  }
+};
+</script>
