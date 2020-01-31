28781a714c9b1f5912bb2f8849b64f9e723c3c60
Hasan Yavuz <hyavuz@bimser.com.tr>
2020-01-31 14:41:19 +0300

    Model updated for save.

--- src/Monitoras.Web/Models/Client/MTMMonitorSave.cs
+++ src/Monitoras.Web/Models/Client/MTMMonitorSave.cs
@@ -1,5 +1,8 @@
+using System;
+
 namespace Monitoras.Web {
     public class MTMMonitorSave {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
     }
